516837b17c6b19303bfbd3088d0194231672909f
Sven Boor <sven.boor@gmail.com>
2019-11-14 23:56:44 +0100

    Change way IMEI is read out.

--- command/login.go
+++ command/login.go
@@ -6,6 +6,6 @@ import (
 )
 
 func ProcessLogin(content []byte) {
-	log.Println("IMEI ", util.BytesToInt(content[1:8]))
-	log.Println("Sequence number: ", util.BytesToInt(content[13:14]))
+	log.Println("IMEI ", util.BytesToInt(content[1:9]))
+	log.Println("Sequence number: ", util.BytesToInt(content[13:15]))
 }
