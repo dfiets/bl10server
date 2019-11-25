0def235b314c4ec0f484363af6c6a0e9b9a3d333
Sven Boor <sven.boor@gmail.com>
2019-11-25 21:39:06 +0100

    Start with grpc server.

---
+++ server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	bl10 "bl10server/bl10comms"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type bl10LockServer struct {
+}
+
+func (s *bl10LockServer) Unlock(ctx context.Context, lock *bl10.Lock) (*bl10.LockStatus, error) {
+
+	// No feature was found, return an unnamed feature
+	return &bl10.LockStatus{IsCharching: true, IsLocked: true, Imei: lock.GetImei()}, nil
+}
+
+func startGrpcServer() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9090))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	bl10.RegisterBL10LockServer(grpcServer, &bl10LockServer{})
+	grpcServer.Serve(lis)
+}
